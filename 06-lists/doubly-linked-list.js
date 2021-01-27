63b5fbe9e516d6a94fe6463f0b4423047fe5a1b0
Ariye Bar-tov <ariyeb@gmail.com>
2021-01-27 20:41:12 +0200

    sll,dll: insert, remove

--- 06-lists/doubly-linked-list.js
+++ 06-lists/doubly-linked-list.js
@@ -46,6 +46,106 @@ class DoublyLinkedList {
         return lastNode;
     }
     // O(1)
+
+    shift() {
+        if (this.head == null) return;
+        let firstNode = this.head;
+        this.size--;
+        if (this.size === 0) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.head = this.head.next;
+            this.head.prev = null;
+            firstNode.next = null;
+        }
+        return firstNode;
+    }
+
+    unShift(value) {
+        const firstNode = new Node(value);
+        if (this.size === 0) {
+            this.head = firstNode;
+            this.tail = firstNode;
+        } else {
+            this.head.prev = firstNode;
+            firstNode.next = this.head;
+            this.head = firstNode;
+        }
+        return this;
+    }
+
+    get(index) {
+        if (index >= this.size || index < 0) return;
+        const middle = Math.floor(this.size / 2);
+        if (index <= middle) {
+            let currenNode = this.head;
+            for (let i = 0; i <= middle; i++) {
+                if (i === index) return currenNode;
+                currenNode = currenNode.next;
+            }
+        } else {
+            let currentNode = this.tail;
+            for (let i = this.size - 1; i > middle; i--) {
+                if (i === index) return currentNode;
+                currentNode = currentNode.prev;
+            }
+        }
+    }
+
+    set(index, value) {
+        const nodeAtIndex = this.get(index);
+        if (nodeAtIndex == undefined) return;
+        nodeAtIndex.value = value;
+        return nodeAtIndex;
+    }
+
+    insert(index, value) {
+        if (index === 0) {
+            this.unShift(value);
+            return this.head;
+        }
+        if (index === this.size) {
+            this.push(value);
+            return this.tail;
+        }
+
+        const nodeAtIndex = this.get(index);
+        if (nodeAtIndex == undefined) return;
+        const nodeBfore = nodeAtIndex.prev;
+        const newNode = new Node(value);
+        nodeBfore.next = newNode;
+        newNode.prev = nodeBfore;
+        newNode.next = nodeAtIndex;
+        nodeAtIndex.prev = newNode;
+        this.size++;
+        return newNode;
+    }
+
+    remove(index) {
+        if (index < 0 || index >= this.size) return;
+        if (index === 0) return this.shift();
+        if (index === this.size - 1) return this.pop();
+        const nodeToRemove = this.get(index);
+        const nodeBfore = nodeToRemove.prev;
+        const nodeAfter = nodeToRemove.next;
+        nodeBfore.next = nodeAfter;
+        nodeAfter.prev = nodeBfore;
+        nodeToRemove.next = null;
+        nodeToRemove.prev = null;
+        size--;
+        return nodeToRemove;
+    }
+
+    toArray() {
+        const arr = [];
+        let currentNode = this.head;
+        while (currentNode != null) {
+            arr.push(currentNode.value);
+            currentNode = currentNode.next;
+        }
+        return arr;
+    }
 }
 
 const dll = new DoublyLinkedList();
@@ -53,4 +153,6 @@ dll.push("Hello");
 dll.push("world");
 dll.push("!");
 dll.pop();
-console.log(dll);
+dll.shift();
+dll.unShift("Hello");
+console.log(dll.toArray());
--- 06-lists/singly-linked-list.js
+++ 06-lists/singly-linked-list.js
@@ -49,8 +49,86 @@ class SinglyLinkedList {
     }
     // O(n)
 
-    toArray(){
-        
+    shift() {
+        if (this.head == null) return;
+
+        const firstNode = this.head;
+        this.head = this.head.next;
+        this.size--;
+        if (this.size === 0) this.tail = null;
+        firstNode.next = null;
+        return firstNode;
+    }
+
+    unShift(value) {
+        const firstNode = new Node(value);
+        firstNode.next = this.head;
+        this.head = firstNode;
+        this.size++;
+        if (this.size === 1) this.tail = firstNode;
+        return this;
+    }
+
+    get(index) {
+        if (index >= this.size || index < 0) return;
+        if (index === this.size - 1) return this.tail;
+
+        let nodeAtIndex = this.head;
+        for (let i = 0; i < index; i++) {
+            nodeAtIndex = nodeAtIndex.next;
+        }
+        return nodeAtIndex;
+    }
+
+    set(index, value) {
+        const nodeAtIndex = this.get(index);
+        if (nodeAtIndex == undefined) return;
+        nodeAtIndex.value = value;
+        return nodeAtIndex;
+    }
+
+    insert(index, value) {
+        if (index === 0) {
+            this.unShift(value);
+            return this.head;
+        }
+        if (index === this.size) {
+            this.push(value);
+            return this.tail;
+        }
+
+        const nodeBefore = this.get(index - 1);
+        if (nodeBefore == undefined) return;
+
+        const nodeAfter = nodeBefore.next;
+        const newNode = new Node(value);
+        nodeBefore.next = newNode;
+        newNode.next = nodeAfter;
+        this.size++;
+        return newNode;
+    }
+
+    remove(index) {
+        if (index < 0 || index >= this.size) return;
+        if (index === 0) return this.shift();
+        if (index === this.size - 1) return this.pop();
+
+        const nodeBefore = this.get(index - 1);
+        const nodeToRemove = nodeBefore.next;
+        nodeBefore.next = nodeToRemove.next;
+        this.size--;
+        nodeToRemove.next = null;
+        return nodeToRemove;
+    }
+
+    toArray() {
+        const arr = [];
+        let currentNode = this.head;
+        while (currentNode != null) {
+            arr.push(currentNode.value);
+            currentNode = currentNode.next;
+        }
+        return arr;
     }
 }
 
@@ -59,4 +137,6 @@ sll.push("Hello");
 sll.push("world");
 sll.push("!");
 sll.pop();
-console.log(sll);
+sll.shift();
+sll.unShift("Hello");
+console.log(sll.toArray());
